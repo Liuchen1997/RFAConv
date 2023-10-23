c604c15d831edd79a92c789657fbd1c1c9cb015e
Zhang Xin <54203800+ZhangXin666888@users.noreply.github.com>
2023-10-23 22:57:12 +0800

    Update model.py

--- model.py
+++ model.py
@@ -8,8 +8,35 @@ import torch
 from torch import nn
 from einops import rearrange
 
+class RFAConv(nn.Module): # 基于Group Conv实现的RFAConv
+    def __init__(self,in_channel,out_channel,kernel_size,stride=1):
+        super().__init__()
+        self.kernel_size = kernel_size
 
-class RFAConv1(nn.Module): # 基于Unfold实现的RFAConv
+        self.get_weight = nn.Sequential(nn.AvgPool2d(kernel_size=kernel_size, padding=kernel_size // 2, stride=stride),
+                                        nn.Conv2d(in_channel, in_channel * (kernel_size ** 2), kernel_size=1, groups=in_channel,bias=False))
+        self.generate_feature = nn.Sequential(
+            nn.Conv2d(in_channel, in_channel * (kernel_size ** 2), kernel_size=kernel_size,padding=kernel_size//2,stride=stride, groups=in_channel, bias=False),
+            nn.BatchNorm2d(in_channel * (kernel_size ** 2)),
+            nn.ReLU())
+       
+        self.conv = nn.Sequential(nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size, stride=kernel_size),
+                                  nn.BatchNorm2d(out_channel),
+                                  nn.ReLU())
+
+    def forward(self,x):
+        b,c = x.shape[0:2]
+        weight =  self.get_weight(x)
+        h,w = weight.shape[2:]
+        weighted = weight.view(b, c, self.kernel_size ** 2, h, w).softmax(2)  # b c*kernel**2,h,w ->  b c k**2 h w 
+        feature = self.generate_feature(x).view(b, c, self.kernel_size ** 2, h, w)  #b c*kernel**2,h,w ->  b c k**2 h w   获得感受野空间特征
+        weighted_data = feature * weighted
+        conv_data = rearrange(weighted_data, 'b c (n1 n2) h w -> b c (h n1) (w n2)', n1=self.kernel_size, # b c k**2 h w ->  b c h*k w*k
+                              n2=self.kernel_size)
+        return self.conv(conv_data)
+
+
+class RFAConv(nn.Module): # 基于Unfold实现的RFAConv
     def __init__(self, in_channel, out_channel, kernel_size=3):
         super().__init__()
         self.kernel_size = kernel_size
@@ -37,32 +64,7 @@ class RFAConv1(nn.Module): # 基于Unfold实现的RFAConv
         return self.act(self.bn(conv_out))
 
  
-class RFAConv2(nn.Module): # 基于Group Conv实现的RFAConv
-    def __init__(self,in_channel,out_channel,kernel_size,stride=1):
-        super().__init__()
-        self.kernel_size = kernel_size
 
-        self.get_weight = nn.Sequential(nn.AvgPool2d(kernel_size=kernel_size, padding=kernel_size // 2, stride=stride),
-                                        nn.Conv2d(in_channel, in_channel * (kernel_size ** 2), kernel_size=1, groups=in_channel,bias=False))
-        self.generate_feature = nn.Sequential(
-            nn.Conv2d(in_channel, in_channel * (kernel_size ** 2), kernel_size=kernel_size,padding=kernel_size//2,stride=stride, groups=in_channel, bias=False),
-            nn.BatchNorm2d(in_channel * (kernel_size ** 2)),
-            nn.ReLU())
-       
-        self.conv = nn.Sequential(nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size, stride=kernel_size),
-                                  nn.BatchNorm2d(out_channel),
-                                  nn.ReLU())
-
-    def forward(self,x):
-        b,c = x.shape[0:2]
-        weight =  self.get_weight(x)
-        h,w = weight.shape[2:]
-        weighted = weight.view(b, c, self.kernel_size ** 2, h, w).softmax(2)  # b c*kernel**2,h,w ->  b c k**2 h w 
-        feature = self.generate_feature(x).view(b, c, self.kernel_size ** 2, h, w)  #b c*kernel**2,h,w ->  b c k**2 h w   获得感受野空间特征
-        weighted_data = feature * weighted
-        conv_data = rearrange(weighted_data, 'b c (n1 n2) h w -> b c (h n1) (w n2)', n1=self.kernel_size, # b c k**2 h w ->  b c h*k w*k
-                              n2=self.kernel_size)
-        return self.conv(conv_data)
 
 
 
